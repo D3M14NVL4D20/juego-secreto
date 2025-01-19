612e701aa12989d9ddd333d107fc4357284e6275
D3M14NVL4D <79564200+D3M14NVL4D20@users.noreply.github.com>
2025-01-18 20:49:57 -0600

    Versión Final Juego

---
+++ wawa.js
@@ -0,0 +1,45 @@
+/*function factorial (numero){
+    let resultado = 1;
+    while (numero > 1) {
+        resultado = resultado*numero;
+        numero--;
+    }  
+    console.log('factorial', resultado);
+}
+factorial(7);
+
+let dolar = 0;
+function conversionMX(dolar){
+   let pesos = 20.50 * dolar;
+   console.log(dolar, `${(dolar === 1) ? 'dólar' : 'dólares'}`, 'son', pesos, 'pesos');
+}
+conversionMX(5);
+
+
+function area (altura, anchura){
+ return resultado = (altura*anchura);
+}
+area(4,9);
+console.log(resultado);
+
+let pi = 3.14;
+function medida(radio){
+   let perimetro = 2 * pi * radio;
+   let area = pi * (radio*radio);
+    console.log('el perimetro es igual a:',perimetro, 'y el area es igual a :', area);
+}
+medida(5);*/
+
+/*function tablita(numero){
+    let numero2 = 0;
+    while(numero2 < 10){
+        numero2 ++;
+    let resultado = numero * numero2;
+    console.log('el resultado de ', numero ,'por', numero2, 'es', resultado);
+    }
+    
+}
+tablita(56);*/
+
+/*let listaGenerica = [];
+let lenguajesDeProgramacion = [JavaScript, C, C++, Kotlin, Python];*/
